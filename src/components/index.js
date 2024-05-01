dadf1d4dfc3f2371424c8c944da43648d605627e
Evan <mr.evan.gordon@gmail.com>
2024-05-01 12:41:12 +0100

    bugs

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
-export { default as Hero } from "./Hero.tsx";
+export { default as Hero } from "./Hero.jsx";
 
 export { default as Projects } from "./Projects";
-export { default as Education } from "./Education.tsx";
+export { default as Education } from "./Education.jsx";
 export { default as About } from "./About.tsx";
 export { default as Navbar } from "./Navbar";
 export { default as ContactForm } from "./ContactForm";
 export { default as Contact } from "./Contact.tsx";
-export { default as Footer } from "./Footer.tsx";
+export { default as Footer } from "./Footer.jsx";
